6063a9d7c290b2af66e5fd92e2877593bc3ca2a3
Chris Jennings <chrisjennings555@gmail.com>
2019-01-16 08:55:06 +0000

    files set up

---
+++ src/test/java/DodgemsTest.java
@@ -0,0 +1,17 @@
+import org.junit.Before;
+import org.junit.Test;
+
+public class DodgemsTest {
+
+    Dodgems dodgems;
+
+    @Before
+    public void before(){
+        dodgems = new Dodgems("Fun Cars");
+    }
+
+    @Test
+    public void dodgemsHaveName(){
+
+    }
+}
